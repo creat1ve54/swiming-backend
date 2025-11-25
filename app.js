12d304388d934052b86a357454a496f926c238b5
arthur <artyr.petrosyan15@mail.ru>
2025-11-25 19:36:02 +0300

    load

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", router);
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 const start = async () => {
   try {
     await sequelize.authenticate();
