5fb1efe51f77f4c51265ace2f13d557bb874b45e
arthur <artyr.petrosyan15@mail.ru>
2025-11-25 19:38:39 +0300

    load

--- app.js
+++ app.js
@@ -6,30 +6,23 @@ const sequelize = require("./db");
 const router = require("./routes/index");
 const models = require("./models/models");
 
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT || 5002;
 
 const corsOptions = {
-  origin: 'http://212.67.9.16:3000', // Укажите точный адрес вашего фронтенда
-  credentials: true, // Если нужно передавать куки/аутентификацию
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 204,
+  preflightContinue: false,
 };
-
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", router);
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
 
 const start = async () => {
   try {
