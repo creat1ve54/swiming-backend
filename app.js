3b16f1db50fcfca2762f473fa03d390fd16bd462
arthur <artyr.petrosyan15@mail.ru>
2025-11-25 18:54:56 +0300

    load

--- app.js
+++ app.js
@@ -12,9 +12,16 @@ const app = express();
 
 const PORT = process.env.PORT || 5002;
 
-app.use(cors({ origin: 'http://212.67.9.16:3000' }));
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://212.67.9.16:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 const start = async () => {
   try {
