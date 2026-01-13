dbd958ed1624e7e81d9dda4bfe907db92ad18f59
arthur <artyr.petrosyan15@mail.ru>
2026-01-13 21:54:47 +0300

    fix

--- models/models.js
+++ models/models.js
@@ -346,11 +346,6 @@ const TournamentName= sequelize.define("tournamentName", {
   date: { type: DataTypes.STRING, defaultValue: "" },
 });
 
-const newTournament = await TournamentName.create({
-  name: "",
-  date: ""
-});
-
 
 
 // AnyTeamProgram.hasMany(Sportsmans, { as: "sportsmansArray" });
